eed1e2bcf1906a8a484c0386cc60a3b4c27b7458
vinci7777 <krzysiek.fitas91@gmail.com>
2023-10-01 16:35:28 +0200

    added info messages for daily check&update methods

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ import streamlit as st
 st.title('Hamza-AI')
 
 def job():
+    print("Running a daily videos check job, please wait.")
     videos = get_channel_videos() 
     if videos:
         save_missing_videos_to_database()
+        print("Addidng missing videos to database...")
+    else:
+        print("No new videos found!")
 
 # Run the scheduler in a loop
 schedule.every().day.do(job)
