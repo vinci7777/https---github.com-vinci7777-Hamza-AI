2ca075c40959fd35de1ff8e50d9d3e657c4289c5
vinci7777 <krzysiek.fitas91@gmail.com>
2023-10-01 18:58:52 +0200

    added logging info text

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ from youtube import get_channel_videos
 import schedule
 import time
 import streamlit as st
+import logging
 
 st.title('Hamza-AI')
 
 def job():
-    print("Running a daily videos check job, please wait.")
-    videos = get_channel_videos() 
-    if videos:
-        save_missing_videos_to_database()
-        print("Adding missing videos to the database...")
-    else:
-        print("No new videos found!")
+    logging.info("Running a daily videos check job, please wait.")
+    try:
+        videos = get_channel_videos() 
+        if videos:
+            save_missing_videos_to_database()
+            logging.info("Adding missing videos to the database...")
+        else:
+            logging.info("No new videos found!")
+    except Exception as e:
+        logging.error(f"Error in job: {str(e)}")
 
 # Define the time you want to run the job (e.g., 10:00 AM)
-desired_time = "16:45"
+desired_time = "19:00"
 
 # Schedule the job to run at the desired time
 schedule.every().day.at(desired_time).do(job)
